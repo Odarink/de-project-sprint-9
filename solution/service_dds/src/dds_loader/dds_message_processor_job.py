ef14be75adb5198af8648cd2c8c042550dec7b57
odarink <holdzhgonov.a@gmail.com>
2024-05-22 21:06:46 +0300

    dds_service first_steps

--- solution/service_dds/src/dds_loader/dds_message_processor_job.py
+++ solution/service_dds/src/dds_loader/dds_message_processor_job.py
@@ -1,16 +1,53 @@
 from datetime import datetime
 from logging import Logger
 
+from lib.kafka_connect import KafkaConsumer, KafkaProducer
+from dds_loader.repository import DdsRepository
 
 class DdsMessageProcessor:
     def __init__(self,
+                 consumer: KafkaConsumer,
+                 producer: KafkaProducer,
+                 dds_repository: DdsRepository,
+                 batch_size: int,
                  logger: Logger) -> None:
-
+        self._consumer = consumer
+        self._producer = producer
+        self._dds_repository = dds_repository
         self._logger = logger
-
-        self._batch_size = 30
+        self._batch_size = batch_size
 
     def run(self) -> None:
         self._logger.info(f"{datetime.utcnow()}: START")
+        for _ in range(self._batch_size):
+            msg = self._consumer.consume()
+            if not msg:
+                break
+            self._logger.info(f"{datetime.utcnow()}: Message received")
+            object_id = msg["object_id"]
+            object_type = msg["object_type"]
+
+            if object_type == 'order':
+                send_dttm = msg["payload"]["date"]
+                cost = msg["payload"]["cost"]
+                payment = msg["payload"]["payment"]
+                status = msg["payload"]["status"]
+                user_id = msg["payload"]["user"]['id']
+                user_name = msg["payload"]["user"]['name']
+                user_login = msg["payload"]["user"]['login']
+                restaurant_id = msg["payload"]["restaurant"]['id']
+                restaurant_name = msg["payload"]["restaurant"]['name']
+                products_list = msg["payload"]["products"]
+                for product in products_list:
+                    self._dds_repository.h_products_insert(product_id=product['id'],
+                                                        sent_dttm=send_dttm,
+                                                        load_src='kafka')
+                self._dds_repository.h_users_insert(user_id=user_id,
+                            sent_dttm=send_dttm,
+                            load_src='kafka')
+
+                self._producer.produce(msg)
+                self._logger.info(f"{datetime.utcnow()}. Message Sent")
+
 
         self._logger.info(f"{datetime.utcnow()}: FINISH")
--- solution/service_dds/src/dds_loader/repository/dds_repository.py
+++ solution/service_dds/src/dds_loader/repository/dds_repository.py
@@ -9,3 +9,61 @@ from pydantic import BaseModel
 class DdsRepository:
     def __init__(self, db: PgConnect) -> None:
         self._db = db
+
+    def h_users_insert(self,
+                            user_id: int,
+                            sent_dttm: datetime,
+                            load_src: str
+                            ) -> None:
+
+        with self._db.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                         INSERT INTO dds.h_user (
+                         h_user_pk, 
+                         user_id, 
+                         load_dt, 
+                         load_src)
+                    VALUES (%(h_user_pk)s, 
+                            %(user_id)s, 
+                            %(load_dt)s, 
+                            %(load_src)s
+                            )
+                    ON CONFLICT (user_id)
+                    DO NOTHING 
+                    """,
+                    {
+                        'h_user_pk': str(uuid.uuid4()),
+                        'user_id': user_id,
+                        'load_dt': sent_dttm,
+                        'load_src': load_src
+                    })
+
+    def h_products_insert(self,
+                          product_id: int,
+                          sent_dttm: datetime,
+                          load_src: str) -> None:
+        with self._db.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO dds.h_product (
+                        h_product_pk, 
+                        product_id, 
+                        load_dt, 
+                        load_src)
+                    VALUES (%(h_product_pk)s, 
+                            %(product_id)s, 
+                            %(load_dt)s, 
+                            %(load_src)s)
+                    ON CONFLICT (product_id)
+                    DO NOTHING
+                    """,
+                    {
+                        'h_product_pk': str(uuid.uuid4()),
+                        'product_id': product_id,
+                        'load_dt': sent_dttm,
+                        'load_src': load_src
+                    }
+                )
