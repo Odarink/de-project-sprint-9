55562dee413e8c2c56ee9b81dc8dfde3e197698c
odarink <holdzhgonov.a@gmail.com>
2024-05-22 23:18:26 +0300

    add new inserts in dds_service

--- solution/service_dds/src/dds_loader/dds_message_processor_job.py
+++ solution/service_dds/src/dds_loader/dds_message_processor_job.py
@@ -4,31 +4,32 @@ from logging import Logger
 from lib.kafka_connect import KafkaConsumer, KafkaProducer
 from dds_loader.repository import DdsRepository
 
-class DdsMessageProcessor:
+
+class DdsMessageProcessor :
     def __init__(self,
                  consumer: KafkaConsumer,
                  producer: KafkaProducer,
                  dds_repository: DdsRepository,
                  batch_size: int,
-                 logger: Logger) -> None:
+                 logger: Logger) -> None :
         self._consumer = consumer
         self._producer = producer
         self._dds_repository = dds_repository
         self._logger = logger
         self._batch_size = batch_size
 
-    def run(self) -> None:
+    def run(self) -> None :
         self._logger.info(f"{datetime.utcnow()}: START")
-        for _ in range(self._batch_size):
+        for _ in range(self._batch_size) :
             msg = self._consumer.consume()
-            if not msg:
+            if not msg :
                 break
             self._logger.info(f"{datetime.utcnow()}: Message received")
             object_id = msg["object_id"]
             object_type = msg["object_type"]
-
-            if object_type == 'order':
-                send_dttm = msg["payload"]["date"]
+            if object_type == 'order' :
+                order_id = msg["payload"]["id"]
+                order_dt = msg["payload"]["date"]
                 cost = msg["payload"]["cost"]
                 payment = msg["payload"]["payment"]
                 status = msg["payload"]["status"]
@@ -38,16 +39,29 @@ class DdsMessageProcessor:
                 restaurant_id = msg["payload"]["restaurant"]['id']
                 restaurant_name = msg["payload"]["restaurant"]['name']
                 products_list = msg["payload"]["products"]
-                for product in products_list:
+                for product in products_list :
                     self._dds_repository.h_products_insert(product_id=product['id'],
-                                                        sent_dttm=send_dttm,
-                                                        load_src='kafka')
+                                                           load_dt=datetime.now(),
+                                                           load_src='kafka')
+                    self._dds_repository.h_category_insert(category_name=product['category'],
+                                                           load_dt=datetime.now(),
+                                                           load_src='kafka')
+                    self._dds_repository.l_order_product(order_id=order_id,
+                                                         product_id=product['id'],
+                                                         load_dt=datetime.now(),
+                                                         load_src='kafka')
                 self._dds_repository.h_users_insert(user_id=user_id,
-                            sent_dttm=send_dttm,
-                            load_src='kafka')
+                                                    load_dt=datetime.now(),
+                                                    load_src='kafka')
+                self._dds_repository.h_order_insert(order_id=order_id,
+                                                    order_dt=order_dt,
+                                                    load_dt=datetime.now(),
+                                                    load_src='kafka')
+                self._dds_repository.h_restaurant_insert(restaurant_id=restaurant_id,
+                                                         load_dt=datetime.now(),
+                                                         load_src='kafka')
 
                 self._producer.produce(msg)
                 self._logger.info(f"{datetime.utcnow()}. Message Sent")
-
 
         self._logger.info(f"{datetime.utcnow()}: FINISH")
--- solution/service_dds/src/dds_loader/repository/dds_repository.py
+++ solution/service_dds/src/dds_loader/repository/dds_repository.py
@@ -12,7 +12,7 @@ class DdsRepository:
 
     def h_users_insert(self,
                             user_id: int,
-                            sent_dttm: datetime,
+                            load_dt: datetime,
                             load_src: str
                             ) -> None:
 
@@ -36,13 +36,13 @@ class DdsRepository:
                     {
                         'h_user_pk': str(uuid.uuid4()),
                         'user_id': user_id,
-                        'load_dt': sent_dttm,
+                        'load_dt': load_dt,
                         'load_src': load_src
                     })
 
     def h_products_insert(self,
                           product_id: int,
-                          sent_dttm: datetime,
+                          load_dt: datetime,
                           load_src: str) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
@@ -63,7 +63,130 @@ class DdsRepository:
                     {
                         'h_product_pk': str(uuid.uuid4()),
                         'product_id': product_id,
-                        'load_dt': sent_dttm,
+                        'load_dt': load_dt,
+                        'load_src': load_src
+                    }
+                )
+
+    def h_category_insert(self,
+                          category_name: int,
+                          load_dt: datetime,
+                          load_src: str) -> None:
+        with self._db.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO dds.h_category (
+                        h_category_pk, 
+                        category_name, 
+                        load_dt, 
+                        load_src)
+                    VALUES (%(h_category_pk)s, 
+                            %(category_name)s, 
+                            %(load_dt)s, 
+                            %(load_src)s)
+                    ON CONFLICT (category_name)
+                    DO NOTHING
+                    """,
+                    {
+                        'h_category_pk': str(uuid.uuid4()),
+                        'category_name': category_name,
+                        'load_dt': load_dt,
+                        'load_src': load_src
+                    }
+                )
+
+    def h_order_insert(self,
+                          order_id: int,
+                          load_dt: datetime,
+                          order_dt: datetime,
+                          load_src: str) -> None:
+        with self._db.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO dds.h_order (
+                        h_order_pk, 
+                        order_id, 
+                        order_dt,
+                        load_dt, 
+                        load_src)
+                    VALUES (%(h_order_pk)s, 
+                            %(order_id)s, 
+                            %(order_dt)s, 
+                            %(load_dt)s,                             
+                            %(load_src)s)
+                    ON CONFLICT (order_id)
+                    DO NOTHING
+                    """,
+                    {
+                        'h_order_pk': str(uuid.uuid4()),
+                        'order_id': order_id,
+                        'load_dt': load_dt,
+                        'order_dt': order_dt,
+                        'load_src': load_src
+                    }
+                )
+
+    def h_restaurant_insert(self,
+                          restaurant_id: int,
+                          load_dt: datetime,
+                          load_src: str) -> None:
+        with self._db.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO dds.h_restaurant (
+                        h_restaurant_pk, 
+                        restaurant_id, 
+                        load_dt, 
+                        load_src)
+                    VALUES (%(h_restaurant_pk)s, 
+                            %(restaurant_id)s, 
+                            %(load_dt)s, 
+                            %(load_src)s)
+                    ON CONFLICT (restaurant_id)
+                    DO NOTHING
+                    """,
+                    {
+                        'h_restaurant_pk': str(uuid.uuid4()),
+                        'restaurant_id': restaurant_id,
+                        'load_dt': load_dt,
+                        'load_src': load_src
+                    }
+                )
+
+    def l_order_product(self,
+                        order_id: int,
+                        product_id: int,
+                        load_dt: datetime,
+                        load_src: str) -> None:
+        with self._db.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO dds.l_order_product (
+                        hk_order_product_pk, 
+                        h_order_pk, 
+                        h_product_pk,
+                        load_dt, 
+                        load_src)
+                    select %(hk_order_product_pk)s, 
+                            o.h_order_pk,
+                            p.h_product_pk,
+                            %(load_dt)s, 
+                            %(load_src)s
+                    from dds.h_order as o
+                        join dds.h_product as p on p.product_id = %(product_id)s
+                    where  o.order_id = %(order_id)s
+                    ON CONFLICT (h_order_pk, h_product_pk)
+                    DO NOTHING
+                    """,
+                    {
+                        'hk_order_product_pk': str(uuid.uuid4()),
+                        'order_id': order_id,
+                        'product_id': product_id,
+                        'load_dt': load_dt,
                         'load_src': load_src
                     }
                 )
